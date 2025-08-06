f714db37085fe45905ee552602f22fe2fe4f6556
razvanmacovei15 <razvanmc15@gmail.com>
2025-08-06 11:25:07 +0300

    starting the DB

---
+++ database/factories/LocationFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Location>
+ */
+class LocationFactory extends Factory
+{
+    protected $states = [
+        'Cluj', 'Timisoara', 'Bucuresti'
+    ];
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->randomElement($this->states),
+            'country' => 'Romania',
+        ];
+    }
+}
