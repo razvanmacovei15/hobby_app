60015240a30fbc60db5e948e74017f29b24ad399
razvanmacovei15 <razvanmc15@gmail.com>
2025-09-10 22:29:23 +0300

    user/journal-entries

--- app/Filament/Resources/Authorization/Roles/Schemas/RoleForm.php
+++ app/Filament/Resources/Authorization/Roles/Schemas/RoleForm.php
@@ -99,6 +99,7 @@ class RoleForm
                         ->gridDirection('row'),
                 ])
                 ->columnSpanFull()
+                ->collapsed(true)
                 ->collapsible();
         }
 
